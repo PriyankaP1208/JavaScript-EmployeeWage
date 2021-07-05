b9727da986ecf58a7e81601ca30081fd029ecd8f
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-07-05 09:42:37 +0530

    ADDED:Ability to validate email id

--- Regex.js
+++ Regex.js
@@ -2,10 +2,21 @@ const prompt = require('prompt-sync')();
 function validatePinCode(pinCode) {
     let pinCodeRegex = RegExp('^[0-9]{3}[ ]?[0-9]{3}$');
     if(pinCodeRegex.test(pinCode))
-        console.log("PinCode is valid!");
+        console.log("PinCode is valid");
     else
-        console.log("PinCode is invalid!");
+        console.log("PinCode is invalid");
 }
 
 let pinCode = prompt("Enter pin code:");
 validatePinCode(pinCode);
+
+function emailvalidation(emailId) {
+    let emailRegex = RegExp('^[a-z]+[-+._]?[a-z]+@[a-z]+[.][a-z]{2,}[.]?([a-z]{2,})?$');
+    if(emailRegex.test(emailId))
+        console.log("Email Id is valid")
+    else
+        console.log("Invalid Email Id!");
+}
+
+let emailId = prompt("Enter email:");
+emailvalidation(emailId);
